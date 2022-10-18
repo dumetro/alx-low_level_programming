db3f563bde496beeca9cd80ee4e2b1062c143e3c
Tendai Muza <muzatendai2@gmail.com>
2022-10-17 19:29:52 -0700

    changes on func write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
   * main - prints "and that piece of art is useful\" - Dora Korpar,
   * 2015-10-19."
   * Description: file that prints text
-  * Return: Exit code 1
+  * Return: Always 1 (success)
  */
 
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 			59);
 	return (1);
 }
