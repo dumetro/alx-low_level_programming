2411279b6fce136137d78f0e3265375752529363
Tendai Muza <muzatendai2@gmail.com>
2022-10-17 17:45:43 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ * show output "with proper grammer, but the outcome is a piece of art"
+ *
+ * Return: Always 0 for success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
