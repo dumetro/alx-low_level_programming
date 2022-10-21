7dee8e11b2809799de70c8372355569afad62fe2
Tendai Muza <muzatendai2@gmail.com>
2022-10-20 17:21:37 -0700

    tasks 0-5

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is between 0 and 9
+ * @c: character to be checked
+ * Return: 1 if it is and 0 if not
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum
+ */
+
+int mul(int a, int b)
+{
+	int sumInts;
+
+	sumInts = a * b;
+	return (sumInts);
+}
