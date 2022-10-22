7329324c14d2b1fa439e52322803b8cdbee0f490
Tendai Muza <muzatendai2@gmail.com>
2022-10-21 17:41:49 -0700

    v1.0.3

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,20 +10,21 @@
 int main(void)
 {
 
-	int num = 1;
+	int num;
 
-	while (num <= 100)
+	for (num = 1; num <= 100; num++)
 	{
 		if (num % 15 == 0)
 			printf("FizzBuzz");
-		else if (num % 5 == 0)
-			printf("Buzz");
 		else if (num % 3 == 0)
 			printf("Fizz");
+		else if (num % 5 == 0)
+			printf("Buzz");
 		else
 			printf("%d", num);
-		printf(" ");
-		num++;
+	if (num == 100)
+		continue;
+	printf(" ");
 	}
 
 	printf("\n");
